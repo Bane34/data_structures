5903ab032f893771e04ec531eed0cc93d97eb5a6
bane34 <bane34.42@gmail.com>
2020-12-24 20:07:19 +0100

    now you can use the queue and the stack

--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 #include "core.h"
 using namespace std;
 
-int main(){
+void menu(void){
+    cout << "Select a data structure to operate in" << endl;
+    cout << "[1] Stack" << endl;
+    cout << "[2] Queue" << endl;
+    cout << "[99] Exit" << endl;
+}
 
+int main(){
+    int res;
+
+    while (true){
+        menu();
+        cout << ">>";
+        cin >> res;
+        switch(res){
+            case 1:
+                mainStack();
+                break;
+            case 2:
+                mainQueue();
+                break;
+            case 99:
+                exit(0);
+            default:
+                cout << "Select a valid option" << endl;
+                continue;
+        }
+    }
 
     return 0;
 }
