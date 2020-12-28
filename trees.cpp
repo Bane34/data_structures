b51da2cec566373038f8123ed9b452bf9af130c5
bane34 <bane34.42@gmail.com>
2020-12-28 19:08:12 +0100

    added more methods

--- trees.cpp
+++ trees.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
 #include <stdlib.h>
+#include <conio.h>
 #include "trees.h"
 using namespace std;
 
+/*
+* Note: this code is for a search binary tree (ABB)
+*/
+
 struct NodeTree {
 	int data;
-	NodeTree* left;
 	NodeTree* right;
+	NodeTree* left;
 };
+
+NodeTree *createNode(int n){
+	NodeTree *new_node = new NodeTree();
+
+	new_node -> data = n;
+	new_node -> right = NULL;
+	new_node -> left = NULL;
+
+	return new_node;
+}
+
+void addTree(NodeTree *&tree, int n){
+	if(tree == NULL){
+		NodeTree *new_node = createNode(n);
+		tree = new_node;
+	}
+	else{
+		int rootValue = tree -> data;
+		if(n < rootValue)
+			addTree(tree -> left, n);
+		else
+			addTree(tree -> right, n);
+	}
+}
+
+void showTree(NodeTree *tree, int counter){
+	
+}
 
 void menuTree(void) {
 	system("cls");
@@ -18,17 +51,27 @@ void menuTree(void) {
 }
 
 void mainTree(void) {
-	cout << "Creating a tree" << endl;
-	NodeTree* tree;
-	cout << "Tree created succesfully!" << endl;
-
 	int res, number;
 
-	switch (res) {
-		case 99:
-			exit(0);
-		default:
-			cout << "Select a valid option" << endl;
-			continue;
+	while (true){
+		menuTree();
+		cout << "Option: ";
+		cin >> res;
+		
+		switch (res) {
+			case 1:
+				cout << "What value do u want to insert: ";
+				cin >> number;
+				addTree(tree, number);
+				cout << number << " added to the tree" << endl;
+				getch();
+				break;
+			case 99:
+				exit(0);
+				break;
+			default:
+				cout << "Select a valid option" << endl;
+				continue;
+		}
 	}
 }
--- trees.h
+++ trees.h
@@ -1,6 +1,12 @@
 #pragma once
 
 struct NodeTree;
+NodeTree *createNode(int);
+NodeTree *tree = NULL;
+
+void addTree(NodeTree *&, int);
+void showTree(NodeTree *, int);
 
 void menuTree(void);
 void mainTree(void);
+
