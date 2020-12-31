24fd66215b02fccf488dd90810100224a6a24e58
bane34 <bane34.42@gmail.com>
2020-12-31 14:41:46 +0100

    now you can removed a node in the tree

--- trees.cpp
+++ trees.cpp
@@ -5,43 +5,108 @@
 using namespace std;
 
 /*
-* Note: this code is for a search binary tree (ABB)
+* Disclaimer: this code is for a search binary tree (ABB)
 * This means that the values higher than the root value will be at the right 
-* And the values lower than the root value will be at the left				 
+* And the values lower than the root value will be at the left
+*
+* Note: to remove a node in the binary tree you need to do a lot of shit
+* I recomend you to investigate how to do it because if I try to explain it
+* I will fuck up
 */
 
 struct NodeTree {
 	int data;
 	NodeTree* right;
 	NodeTree* left;
+	NodeTree* father;
 };
 
-NodeTree *createNode(int n){
+NodeTree *createNode(NodeTree *father, int n){
 	NodeTree *new_node = new NodeTree();
 
 	new_node -> data = n;
 	new_node -> right = NULL;
 	new_node -> left = NULL;
+	new_node -> father = father;
 
 	return new_node;
 }
 
-void addTree(NodeTree *&tree, int n){
+NodeTree *minimun(NodeTree* tree) {
+	if (tree == NULL)
+		return NULL;
+	if (tree -> right)
+		return minimun(tree -> right);
+	else
+		return tree;
+}
+
+void addTree(NodeTree *&tree, NodeTree *father, int n){
 	if(tree == NULL){
-		NodeTree *new_node = createNode(n);
+		NodeTree *new_node = createNode(father, n);
 		tree = new_node;
 	}
 	else{
 		int rootValue = tree -> data;
 		if(n < rootValue)
-			addTree(tree -> left, n);
+			addTree(tree -> left, tree, n);
 		else
-			addTree(tree -> right, n);
+			addTree(tree -> right, tree, n);
 	}
 }
 
-void removeTree(NodeTree *&tree, int n){
-	return;
+void destroyNode(NodeTree *node) {
+	node -> left = NULL;
+	node -> right = NULL;
+
+	delete node;
+}
+
+void replace(NodeTree *tree, NodeTree *newNode) {
+	if (tree -> father) {
+		if (tree -> data == tree -> father -> left -> data) {
+			tree -> father -> left = newNode;
+		}
+		else if (tree ->data == tree-> father -> right -> data) {
+			tree -> father -> right = newNode;
+		}
+	}
+	if (newNode) {
+		newNode -> father = tree -> father;
+	}
+}
+
+void removeTree(NodeTree *tree, int n){
+	//with this function we search the node with the element in the tree
+	if (tree == NULL)
+		return;
+	else if (n < tree -> data)
+		removeTree(tree -> left, n);
+	else if (n > tree -> data)
+		removeTree(tree -> right, n);
+	else
+		removeNode(tree);
+}
+
+void removeNode(NodeTree *removedNode) {
+	//With this function we remove the node previously found by the removeTree function
+	if (removedNode -> left && removedNode -> right) {
+		NodeTree *min = minimun(removedNode -> right);
+		removedNode -> data = min -> data;
+		removeNode(min);
+	}
+	else if (removedNode -> left) {
+		replace(removedNode, removedNode -> left);
+		destroyNode(removedNode);
+	}
+	else if (removedNode -> right) {
+		replace(removedNode, removedNode -> right);
+		destroyNode(removedNode);
+	}
+	else { //If the node isnt a father node, we just replace the node by NULL
+		replace(removedNode, NULL);
+		destroyNode(removedNode);
+	}
 }
 
 void showTree(NodeTree *tree, int counter){
@@ -116,7 +181,7 @@ void mainTree(void) {
 			case 1:
 				cout << "What value do u want to insert: ";
 				cin >> number;
-				addTree(tree, number);
+				addTree(tree, NULL, number);
 				cout << number << " added to the tree" << endl;
 				getch();
 				break;
--- trees.h
+++ trees.h
@@ -1,10 +1,14 @@
 #pragma once
 
 struct NodeTree;
-NodeTree *createNode(int);
+NodeTree *createNode(NodeTree *, int);
+NodeTree *minimun(NodeTree *);
 
-void addTree(NodeTree *&, int);
-void removeTree(NodeTree *&, int);
+void addTree(NodeTree *&, NodeTree *, int);
+void destroyNode(NodeTree *);
+void replace(NodeTree *, NodeTree *);
+void removeTree(NodeTree *, int);
+void removeNode(NodeTree *);
 void showTree(NodeTree *, int);
 bool searchTree(NodeTree *, int);
 void preOrder(NodeTree *);
